69fc3e5bc3ec98841e059c84a8f1dedee41a6314
ArdeshirV <e-job@protonmail.com>
2025-09-08 09:08:48 +0200

    Update

--- go-solati/run-mysql.sh
+++ go-solati/run-mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run --rm --name mysql-db -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mysql:latest
+docker run --rm --name mariadb -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mariadb:latest
 
