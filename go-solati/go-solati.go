e5b08d725552432dcf49a3f141b5169c5cd01d38
ArdeshirV <ArdeshirV@protonmail.com>
2025-09-15 18:36:27 +0000

    Update

--- go-solati/go-solati.go
+++ go-solati/go-solati.go
@@ -637,6 +637,20 @@ func impllementErrorChekers() {
 		panic(err)
 	}
 
+	if err := oneError(); err != nil {
+		panic(err)
+	}
+
+	if _, err := oneResultAndError(); err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := oneError(); err != nil {
+			panic(err)
+		}
+	}()
+
 	res, err := oneResultAndError()
 	if err != nil {
 		panic(err)
