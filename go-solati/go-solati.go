2ec4c4f16e8e8c5f7832395302e38f309b71a8b6
ArdeshirV <ArdeshirV@protonmail.com>
2025-09-22 15:21:11 +0000

    Update

--- go-solati/go-solati.go
+++ go-solati/go-solati.go
@@ -170,7 +170,7 @@ func mainConversion() {
 	fmt.Printf("%s = %x\n", strstring, strstring)
 
 	char := 'ش'
-	fmt.Printf("%c = %U\n", char, char)
+	fmt.Printf("%c = %U, %t, %p\n", char, char, true, &char)
 }
 
 func redisSandBox() {
@@ -216,9 +216,9 @@ func gRPC01() {
 		panic(err)
 	}
 
-    if err := s.Serve(lis); err != nil {
-        panic(err)
-    }
+	if err := s.Serve(lis); err != nil {
+		panic(err)
+	}
 }
 
 type server struct {
@@ -692,7 +692,7 @@ func mainZipArchive() {
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< Updated upstream
+
 	text := "This is a sample text file that is created in Go programming language"
 	if _, err := file.Write([]byte(text)); err != nil {
 		panic(err)
@@ -770,9 +770,6 @@ func impllementErrorChekers() {
 	}()
 
 	fmt.Println(res)
-=======
-	file.Write([]byte("This is a sample text file that is created in Go programming language"))
->>>>>>> Stashed changes
 }
 
 func mainPlugin() {
@@ -2254,10 +2251,7 @@ func mainXX() {
 	mainPlugin()
 	mainZipArchive()
 	mainNext()
-<<<<<<< Updated upstream
 	impllementErrorChekers()
-=======
->>>>>>> Stashed changes
 	mainDesignPattern()
 	mainDataStructures()
 	mainPractice()
