56d6b40a008e640d50c87ce4d3eb02dcea79c11e
ArdeshirV <e-job@protonmail.com>
2025-10-01 14:30:42 +0330

    Update

--- go-solati/go-solati.go
+++ go-solati/go-solati.go
@@ -281,9 +281,15 @@ func gRPC01() {
 	}
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 	if err := s.Serve(lis); err != nil {
 		panic(err)
 	}
+=======
+    if err := s.Serve(lis); err != nil {
+        panic(err)
+    }
+>>>>>>> Stashed changes
 =======
     if err := s.Serve(lis); err != nil {
         panic(err)
@@ -763,6 +769,7 @@ func mainZipArchive() {
 		panic(err)
 	}
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 	text := "This is a sample text file that is created in Go programming language"
 	if _, err := file.Write([]byte(text)); err != nil {
@@ -853,6 +860,9 @@ func impllementErrorChekers() {
 	}()
 
 	fmt.Println(res)
+=======
+	file.Write([]byte("This is a sample text file that is created in Go programming language"))
+>>>>>>> Stashed changes
 =======
 	file.Write([]byte("This is a sample text file that is created in Go programming language"))
 >>>>>>> Stashed changes
---
+++ learning-by-ai/golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/ArdeshirV/books/learning-by-ai/golang
+
+go 1.25.0
