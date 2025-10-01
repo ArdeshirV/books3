6590105a7a947aded2f984f73ce2d98861a6a47b
ArdeshirV <e-job@protonmail.com>
2025-10-01 14:34:30 +0330

    Update

--- go-solati/go-solati.go
+++ go-solati/go-solati.go
@@ -280,27 +280,9 @@ func gRPC01() {
 		panic(err)
 	}
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	if err := s.Serve(lis); err != nil {
 		panic(err)
 	}
-=======
-    if err := s.Serve(lis); err != nil {
-        panic(err)
-    }
->>>>>>> Stashed changes
-=======
-    if err := s.Serve(lis); err != nil {
-        panic(err)
-    }
->>>>>>> Stashed changes
-=======
-    if err := s.Serve(lis); err != nil {
-        panic(err)
-    }
->>>>>>> Stashed changes
 }
 
 type server struct {
@@ -774,9 +756,6 @@ func mainZipArchive() {
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
 	text := "This is a sample text file that is created in Go programming language"
 	if _, err := file.Write([]byte(text)); err != nil {
@@ -867,15 +846,6 @@ func impllementErrorChekers() {
 	}()
 
 	fmt.Println(res)
-=======
-	file.Write([]byte("This is a sample text file that is created in Go programming language"))
->>>>>>> Stashed changes
-=======
-	file.Write([]byte("This is a sample text file that is created in Go programming language"))
->>>>>>> Stashed changes
-=======
-	file.Write([]byte("This is a sample text file that is created in Go programming language"))
->>>>>>> Stashed changes
 }
 
 func mainPlugin() {
