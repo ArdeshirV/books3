c58f47e6302e6cc2979ccc2378cb4af5b65106bf
ArdeshirV <e-job@protonmail.com>
2025-10-01 14:35:58 +0330

    Update

--- go-solati/go-solati.go
+++ go-solati/go-solati.go
@@ -279,16 +279,6 @@ func gRPC01() {
 	if err := s.Serve(lis); err != nil {
 		panic(err)
 	}
-
-<<<<<<< Updated upstream
-	if err := s.Serve(lis); err != nil {
-		panic(err)
-	}
-=======
-    if err := s.Serve(lis); err != nil {
-        panic(err)
-    }
->>>>>>> Stashed changes
 }
 
 type server struct {
@@ -762,7 +752,6 @@ func mainZipArchive() {
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< Updated upstream
 
 	text := "This is a sample text file that is created in Go programming language"
 	if _, err := file.Write([]byte(text)); err != nil {
@@ -853,9 +842,6 @@ func impllementErrorChekers() {
 	}()
 
 	fmt.Println(res)
-=======
-	file.Write([]byte("This is a sample text file that is created in Go programming language"))
->>>>>>> Stashed changes
 }
 
 func mainPlugin() {
